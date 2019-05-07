52ee47bb6769b58f44e69ef95feee48bea515ead
George  Pianka <gpianka1@gmail.com>
2019-05-07 06:16:04 +0000

    Automatically backed up by Learn

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -6,5 +6,13 @@ class GithubRepo
     @name = hash["name"]
     @url = hash["html_url"]
   end
+  
+  def authenticate_user(github_client)
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{github_client]}&scope=repo"
+  end
+
+   def logged_in?(token)
+    !!token
+  end
 
 end
