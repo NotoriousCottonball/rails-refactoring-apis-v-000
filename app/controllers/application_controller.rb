58da79343f671ff2398030276f16527c1c8cad02
George  Pianka <gpianka1@gmail.com>
2019-05-07 06:41:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,11 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      user = GithubRepo.new
-      user.authenticate(ENV['GITHUB_CLIENT']) if user.logged_in?(session[:token])
+      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+    end
+
+    def logged_in?
+      !!session[:token]
     end
     
 end
